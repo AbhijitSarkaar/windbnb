cac5bfcbe78cb9473f6ce5c9d1ad6881d65e92ed
AbhijitSarkaar <theabhijitsarkar@gmail.com>
2022-02-18 17:14:17 +0530

    result title and count

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -9,12 +9,30 @@ const Home = (props) => {
         <HomeContainer>
             <Brand></Brand>
             <SearchBar handleModal={handleModal}></SearchBar>
+            <SearchResultTitle>
+                <Text>Stays in Finland</Text>
+                <Count>12+ stays</Count>
+            </SearchResultTitle>
         </HomeContainer>
     );
 };
 
 const HomeContainer = styled.div`
     padding: 22px 12px 0px 12px;
+`;
+
+const SearchResultTitle = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+const Text = styled.div`
+    font-size: 18px;
+    font-weight: 700;
+    color: #333333;
+`;
+const Count = styled.div`
+    font-size: 14px;
+    color: #4f4f4f;
 `;
 
 export default Home;
